1180e6030712aa939fe200ac6076bf4e4d12f022
PhanNgocTuan <129782979+PNT1319star@users.noreply.github.com>
2024-04-12 00:11:51 +0300

    success

--- Step_2_Finish/server/src/processing/specificcommands/AbstractCommand.java
+++
@@ -1,5 +0,0 @@
-package processing.specificcommands;
-
-public abstract class AbstractCommand {
-    public abstract void execute(Object object);
-}
--- Step_2_Finish/server/src/processor/specificcommands/ServerAbstractCommand.java
+++
@@ -1,8 +0,0 @@
-package processor.specificcommands;
-
-import java.io.IOException;
-import java.net.Socket;
-
-public abstract class ServerAbstractCommand {
-    public abstract byte[] execute(Object object);
-}
