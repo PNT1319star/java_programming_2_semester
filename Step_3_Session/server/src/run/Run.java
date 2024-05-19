1312a7b0026774142149fdbf31104cec39bbd9ac
PhanNgocTuan <129782979+PNT1319star@users.noreply.github.com>
2024-05-19 15:45:51 +0300

    finish

--- Step_3_Session/server/src/run/Run.java
+++ Step_3_Session/server/src/run/Run.java
@@ -4,12 +4,17 @@ import server.Server;
 
 public class Run {
     public static void main(String[] args) {
-//        String port = args[0];
-        String databaseHost = "s374807";
-        String databasePassword = "9cs4jxVhIg4Vqi8D";
-        String databaseAddress = "jdbc:postgresql://localhost:5656/studs";
-        String port = "2222";
-        Server server = new Server(port, databaseAddress, databaseHost, databasePassword);
+        String port = args[0];
+        String databaseHost = args[1];
+        String databasePassword = args[2];
+        String databaseUser = "s374807";
+        String databaseAddress = "jdbc:postgresql://" + databaseHost + ":5432/studs";
+//        String databaseHost = "localhost";
+//        String databaseUser = "s374807";
+//        String databasePassword = "9cs4jxVhIg4Vqi8D";
+//        String databaseAddress = "jdbc:postgresql://" + databaseHost + ":5656/studs";
+//        String port = "2222";
+        Server server = new Server(port, databaseAddress, databaseUser, databasePassword);
         server.run();
     }
 }
