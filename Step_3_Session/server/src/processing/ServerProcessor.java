f15958cfc9411c59c4ba620beb161dd29830b204
PhanNgocTuan <129782979+PNT1319star@users.noreply.github.com>
2024-05-21 18:34:45 +0300

    finish

--- Step_3_Session/server/src/processing/ServerProcessor.java
+++ Step_3_Session/server/src/processing/ServerProcessor.java
@@ -39,68 +39,63 @@ public class ServerProcessor {
     public void decodeAndProcessCommand() {
         //Request reader task
         requestReader.execute(() -> {
-            while (true) {
-                try {
-                    Request request = receiver.receive();
-                    if (request != null) requestQueue.put(request);
-                } catch (IOException | ClassNotFoundException ignored) {
-                } catch (InterruptedException e) {
-                    ConsolePrinter.printError("Interrupted while waiting for request: " + e.getMessage());
-                    throw new RuntimeException(e);
-                }
+            try {
+                Request request = receiver.receive();
+                if (request != null) requestQueue.put(request);
+            } catch (IOException | ClassNotFoundException ignored) {
+
+            } catch (InterruptedException e) {
+                ConsolePrinter.printError("Interrupted while waiting for request: " + e.getMessage());
+                throw new RuntimeException(e);
             }
         });
 
         //Request processor task
         for (int i = 0; i < 5; i++) {
             requestProcessor.execute(() -> {
-                while (true) {
-                    try {
-                        Request request = requestQueue.take();
-                        Response response;
-                        if (request.getSessionId().isEmpty()) {
-                            HashMap<String, AbstractCommand> commandHashMap = Invoker.getRoleCommandsList().get("server");
-                            response = new Response(Invoker.executeCommand(request, commandHashMap));
-                        } else {
-                            String sessionId = request.getSessionId();
-                            int userId = databaseSessionManager.getUserIdBySessionId(sessionId);
-                            CollectionManager.setUsername(userId);
-                            List<String> functionList = databaseSessionManager.getFunctionsList(sessionId);
-                            HashMap<String, AbstractCommand> commandHashMap = new HashMap<>();
-                            for (String function : functionList) {
-                                HashMap<String, AbstractCommand> smallCommandsList = Invoker.getRoleCommandsList().get(function);
-                                commandHashMap.putAll(smallCommandsList);
-                            }
-                            try {
-                                response = new Response(Invoker.executeCommand(request, commandHashMap));
-                            } catch (NullPointerException exception) {
-                                response = new Response("You can not use this command!");
-                            }
+                try {
+                    Request request = requestQueue.take();
+                    Response response;
+                    if (request.getSessionId().isEmpty()) {
+                        HashMap<String, AbstractCommand> commandHashMap = Invoker.getRoleCommandsList().get("server");
+                        response = new Response(Invoker.executeCommand(request, commandHashMap));
+                    } else {
+                        String sessionId = request.getSessionId();
+                        int userId = databaseSessionManager.getUserIdBySessionId(sessionId);
+                        CollectionManager.setUsername(userId);
+                        List<String> functionList = databaseSessionManager.getFunctionsList(sessionId);
+                        HashMap<String, AbstractCommand> commandHashMap = new HashMap<>();
+                        for (String function : functionList) {
+                            HashMap<String, AbstractCommand> smallCommandsList = Invoker.getRoleCommandsList().get(function);
+                            commandHashMap.putAll(smallCommandsList);
                         }
-                        responseQueue.put(response);
-                    } catch (InterruptedException exception) {
-                        ConsolePrinter.printError("Interrupted while processing request: " + exception.getMessage());
-                        throw new RuntimeException(exception);
-                    } catch (HandlingDatabaseException exception) {
-                        ConsolePrinter.printError("Something when wrong in the database");
+                        try {
+                            response = new Response(Invoker.executeCommand(request, commandHashMap));
+                        } catch (NullPointerException exception) {
+                            response = new Response("You can not use this command!");
+                        }
                     }
+                    responseQueue.put(response);
+                } catch (InterruptedException exception) {
+                    ConsolePrinter.printError("Interrupted while processing request: " + exception.getMessage());
+                    throw new RuntimeException(exception);
+                } catch (HandlingDatabaseException exception) {
+                    ConsolePrinter.printError("Something when wrong in the database");
                 }
             });
         }
 
         //Response sender task
         responseSender.execute(() -> {
-            while (true) {
-                try {
-                    Response response = responseQueue.take();
-                    sender.send(response);
-                } catch (IOException e) {
-                    ConsolePrinter.printError("Error sending response: " + e.getMessage());
-                    throw new RuntimeException(e);
-                } catch (InterruptedException e) {
-                    ConsolePrinter.printError("Interrupted while sending response: " + e.getMessage());
-                    throw new RuntimeException(e);
-                }
+            try {
+                Response response = responseQueue.take();
+                sender.send(response);
+            } catch (IOException e) {
+                ConsolePrinter.printError("Error sending response: " + e.getMessage());
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                ConsolePrinter.printError("Interrupted while sending response: " + e.getMessage());
+                throw new RuntimeException(e);
             }
         });
     }
